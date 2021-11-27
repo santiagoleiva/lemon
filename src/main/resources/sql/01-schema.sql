77c543c84406dc4bca612e2a7d0d5a4e6b635ba0
Santiago Leiva <santiagoeleiva@gmail.com>
2021-11-27 10:42:11 -0300

    build(setup): modifica scripts SQL

--- src/main/resources/sql/01-schema.sql
+++ src/main/resources/sql/01-schema.sql
@@ -1,14 +1,25 @@
-create table lemon.user
+create table lemon.usuario
 (
     id       int auto_increment primary key,
     nombre   varchar(50) not null,
     apellido varchar(50) not null,
     alias    varchar(30) not null,
     email    varchar(60) not null,
-    constraint user_alias_uindex
+    constraint usuario_alias_uindex
         unique (alias),
-    constraint user_email_uindex
+    constraint usuario_email_uindex
         unique (email),
-    constraint user_id_uindex
+    constraint usuario_id_uindex
         unique (id)
+);
+
+create table lemon.moneda
+(
+    id          smallint auto_increment
+        primary key,
+    codigo      varchar(6)         not null,
+    descripcion varchar(30)        null,
+    decimales   smallint default 0 not null,
+    constraint moneda_codigo_uindex
+        unique (codigo)
 );
