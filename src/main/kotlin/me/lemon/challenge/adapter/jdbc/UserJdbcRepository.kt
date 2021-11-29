3d7783d16a8be2cb7867c41e73792de0c431c3de
Santiago Leiva <santiagoeleiva@gmail.com>
2021-11-29 08:01:51 -0300

    feat(crear usuario): modifica interfaz de repositorio JDBC

--- src/main/kotlin/me/lemon/challenge/adapter/jdbc/UserJdbcRepository.kt
+++ src/main/kotlin/me/lemon/challenge/adapter/jdbc/UserJdbcRepository.kt
@@ -1,8 +1,8 @@
 package me.lemon.challenge.adapter.jdbc
 
 import me.lemon.challenge.adapter.jdbc.model.UserJdbcModel
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserJdbcRepository : CrudRepository<UserJdbcModel, Int>
+interface UserJdbcRepository : PagingAndSortingRepository<UserJdbcModel, Int>
