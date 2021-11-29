62adf6e9bdd027845d67e08b6c505c817a8c12d3
Santiago Leiva <santiagoeleiva@gmail.com>
2021-11-29 08:29:18 -0300

    refactor(crear usuario): renombra dependencias en caso de uso

--- src/main/kotlin/me/lemon/challenge/application/usecase/CreateUserUseCase.kt
+++ src/main/kotlin/me/lemon/challenge/application/usecase/CreateUserUseCase.kt
@@ -4,31 +4,37 @@ import me.lemon.challenge.application.port.`in`.CreateUserPortIn
 import me.lemon.challenge.application.port.out.QueryCurrencyPortOut
 import me.lemon.challenge.application.port.out.UpsertUserPortOut
 import me.lemon.challenge.domain.Balance
+import me.lemon.challenge.domain.Currency
 import me.lemon.challenge.domain.User
 import org.springframework.stereotype.Component
 import java.math.BigDecimal
 
 @Component
 class CreateUserUseCase(
-    private val userJdbcAdapter: UpsertUserPortOut,
-    private val currencyJdbcAdapter: QueryCurrencyPortOut
+    private val upsertUserAdapter: UpsertUserPortOut,
+    private val queryCurrencyAdapter: QueryCurrencyPortOut
 ) : CreateUserPortIn {
 
     override fun execute(command: CreateUserPortIn.Command): User {
         val user = command.toDomain()
         user.addToWallet(getInitialBalances())
-        return userJdbcAdapter.create(user)
+        return upsertUserAdapter.create(user)
     }
 
-    private fun getInitialBalances(): List<Balance> = currencyJdbcAdapter
+    private fun getInitialBalances(): List<Balance> = queryCurrencyAdapter
         .query()
-        .map { Balance(it, BigDecimal.ZERO) }
+        .map { it toBalanceWithAmount BigDecimal.ZERO }
 
     private fun CreateUserPortIn.Command.toDomain(): User = User(
         firstname = this.firstname,
         lastname = this.lastname,
         alias = this.alias,
         email = this.email
+    )
+
+    private infix fun Currency.toBalanceWithAmount(amount: BigDecimal): Balance = Balance(
+        currency = this,
+        amount = amount
     )
 
     private infix fun User.addToWallet(balances: List<Balance>) = this.wallet.addAll(balances)
