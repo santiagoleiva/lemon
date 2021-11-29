614384ff69298db574fa0da9b3e21779b0b36f95
Santiago Leiva <santiagoeleiva@gmail.com>
2021-11-29 09:42:08 -0300

    test(crear usuario): agrega pruebas de `CurrencyJdbcAdapter`
    - Agrega mock factory.

---
+++ src/test/kotlin/me/lemon/challenge/adapter/jdbc/CurrencyJdbcAdapterTest.kt
@@ -0,0 +1,36 @@
+package me.lemon.challenge.adapter.jdbc
+
+import me.lemon.challenge.adapter.mock.CurrencyMockFactory
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito.*
+import org.mockito.junit.jupiter.MockitoExtension
+
+@DisplayName("Currency Jdbc Adapter Tests")
+@ExtendWith(MockitoExtension::class)
+class CurrencyJdbcAdapterTest {
+
+    private val currencyJdbcRepository: CurrencyJdbcRepository = mock(CurrencyJdbcRepository::class.java)
+
+    private val currencyAdapter = CurrencyJdbcAdapter(
+        currencyJdbcRepository = currencyJdbcRepository
+    )
+
+    @Test
+    @DisplayName("When query() is executed, CurrencyJdbcRepository.findAll() should be invoked")
+    fun testQueryCurrencies() {
+        val mockedJdbcCurrencies = CurrencyMockFactory.currenciesJdbc()
+        val expectedCurrency = CurrencyMockFactory.sampleCurrency()
+
+        `when`(currencyJdbcRepository.findAll()).thenReturn(mockedJdbcCurrencies)
+
+        val result = currencyAdapter.query()
+
+        assertThat(result).contains(expectedCurrency)
+
+        verify(currencyJdbcRepository).findAll()
+    }
+
+}
---
+++ src/test/kotlin/me/lemon/challenge/adapter/mock/CurrencyMockFactory.kt
@@ -0,0 +1,36 @@
+package me.lemon.challenge.adapter.mock
+
+import me.lemon.challenge.adapter.jdbc.model.CurrencyJdbcModel
+import me.lemon.challenge.domain.Currency
+
+object CurrencyMockFactory {
+
+    @JvmStatic
+    private val defaultId: Int = 1
+
+    @JvmStatic
+    private val defaultCode: String = "currency-code-test"
+
+    @JvmStatic
+    private val defaultDescription: String = "currency-description-test"
+
+    @JvmStatic
+    private val defaultDecimals: Int = 2
+
+    fun sampleCurrency(): Currency = Currency(
+        id = defaultId,
+        code = defaultCode,
+        description = defaultDescription,
+        decimals = defaultDecimals
+    )
+
+    fun sampleCurrencyJdbc(): CurrencyJdbcModel = CurrencyJdbcModel(
+        id = defaultId,
+        code = defaultCode,
+        description = defaultDescription,
+        decimals = defaultDecimals
+    )
+
+    fun currenciesJdbc(): List<CurrencyJdbcModel> = listOf(sampleCurrencyJdbc())
+
+}
