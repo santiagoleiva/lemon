a2a0d39fd325ef924113a0bd43966cc8a8367ba5
Santiago Leiva <santiagoeleiva@gmail.com>
2021-11-29 08:39:24 -0300

    feat(crear usuario): agrega valores en catálogo de errores

--- src/main/kotlin/me/lemon/challenge/adapter/controller/ErrorHandler.kt
+++ src/main/kotlin/me/lemon/challenge/adapter/controller/ErrorHandler.kt
@@ -3,6 +3,7 @@ package me.lemon.challenge.adapter.controller
 import me.lemon.challenge.adapter.controller.model.ApiErrorControllerModel
 import me.lemon.challenge.config.ErrorCatalog
 import me.lemon.challenge.config.exception.GenericException
+import me.lemon.challenge.config.exception.UnprocessableEntityException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
@@ -34,6 +35,17 @@ class ErrorHandler {
             message = exception.message
         )
         return ResponseEntity.internalServerError().body(error)
+    }
+
+    @ExceptionHandler(UnprocessableEntityException::class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    fun handleUnprocessableEntity(exception: UnprocessableEntityException): ResponseEntity<ApiErrorControllerModel> {
+        logger.error("Handling unprocessable entity: {}", exception.message, exception)
+        val error = ApiErrorControllerModel(
+            code = exception.code,
+            message = exception.message
+        )
+        return ResponseEntity.unprocessableEntity().body(error)
     }
 
     companion object {
--- src/main/kotlin/me/lemon/challenge/config/ErrorCatalog.kt
+++ src/main/kotlin/me/lemon/challenge/config/ErrorCatalog.kt
@@ -4,6 +4,8 @@ enum class ErrorCatalog(
     val defaultMessage: String
 ) {
 
+    USER_ALIAS_NOT_AVAILABLE("Alias de usuario no disponible"),
+    USER_EMAIL_PREVIOUSLY_REGISTERED("El email está siendo utilizado"),
     INTERNAL_ERROR("Ha ocurrido un error inesperado"),
     UNPROCESSABLE_ENTITY("No es posible procesar la entidad");
 
