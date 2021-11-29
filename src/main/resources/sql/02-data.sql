7b3e17f9474d731516b37bae2d51a8d7714d1ec6
Santiago Leiva <santiagoeleiva@gmail.com>
2021-11-29 20:31:27 -0300

    build(obtener usuario): modifica scripts SQL
    Agrega datos de prueba.

--- src/main/resources/sql/02-data.sql
+++ src/main/resources/sql/02-data.sql
@@ -1,4 +1,14 @@
+insert into lemon.user(firstname, lastname, alias, email)
+values ('Alan', 'Castillo', 'acastillo', 'alan.castillo@lemon.me'),
+       ('Melanie', 'Villalobos', 'mvillalobos', 'melanie.villalobos@lemon.me'),
+       ('Mauro', 'Gonzalez', 'mgonzalez', 'mauro.gonzalez@lemon.me');
+
 insert into lemon.currency(code, description, decimals)
 values ('ARS', 'Peso Argentino', 2),
        ('USDT', 'Tether', 2),
        ('BTC', 'Bitcoin', 8);
+
+insert into lemon.user_wallet(user_id, currency_id, balance)
+select u.id, c.id, 0
+from lemon.currency as c,
+     lemon.user as u
