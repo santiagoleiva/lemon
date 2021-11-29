9f464928624fa53ab82f118ecb937c0d0a23c25f
Santiago Leiva <santiagoeleiva@gmail.com>
2021-11-29 08:43:41 -0300

    feat(crear usuario): define nuevas excepciones

---
+++ src/main/kotlin/me/lemon/challenge/config/exception/UserAliasUnavailableException.kt
@@ -0,0 +1,8 @@
+package me.lemon.challenge.config.exception
+
+import me.lemon.challenge.config.ErrorCatalog
+
+class UserAliasUnavailableException(
+    override val code: String = ErrorCatalog.USER_ALIAS_NOT_AVAILABLE.name,
+    override val message: String = ErrorCatalog.USER_ALIAS_NOT_AVAILABLE.defaultMessage
+) : UnprocessableEntityException(code, message)
---
+++ src/main/kotlin/me/lemon/challenge/config/exception/UserEmailUnavailableException.kt
@@ -0,0 +1,8 @@
+package me.lemon.challenge.config.exception
+
+import me.lemon.challenge.config.ErrorCatalog
+
+class UserEmailUnavailableException(
+    override val code: String = ErrorCatalog.USER_EMAIL_PREVIOUSLY_REGISTERED.name,
+    override val message: String = ErrorCatalog.USER_EMAIL_PREVIOUSLY_REGISTERED.defaultMessage
+) : UnprocessableEntityException(code, message)
