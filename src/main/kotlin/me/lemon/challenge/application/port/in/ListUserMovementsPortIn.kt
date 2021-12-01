897b9e3be057ac99f6aca924a402c707d8904dae
Santiago Leiva <santiagoeleiva@gmail.com>
2021-12-01 15:09:28 -0300

    feat(listar movimientos): define interfaz de puerto de entrada

---
+++ src/main/kotlin/me/lemon/challenge/application/port/in/ListUserMovementsPortIn.kt
@@ -0,0 +1,16 @@
+package me.lemon.challenge.application.port.`in`
+
+import me.lemon.challenge.domain.Movement
+
+interface ListUserMovementsPortIn {
+
+    fun execute(command: Command): List<Movement>
+
+    data class Command(
+        val limit: Int,
+        val offset: Int,
+        val movementType: String,
+        val currency: String
+    )
+
+}
